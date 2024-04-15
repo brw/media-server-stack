ea9fd2dc800c8a983abbb5348d564f91b25ef853
Bas van den Wollenberg <hi@bas.sh>
2024-04-15 21:48:46 +0200

    Add scrutiny

--- src/index.ts
+++ src/index.ts
@@ -370,3 +370,42 @@ const grafanaService = await ContainerService.create("grafana", {
     user: "1000:1000",
   },
 });
+
+const scrutinyService = await ContainerService.create("scrutiny", {
+  image: "ghcr.io/analogj/scrutiny:master-omnibus",
+  webPort: 8080,
+  mounts: [
+    dockerConfMount("scrutiny/config", "/opt/scrutiny/config"),
+    dockerConfMount("scrutiny/influxdb", "/opt/scrutiny/influxdb"),
+    {
+      source: "/run/udev",
+      target: "/run/udev",
+      type: "bind",
+      readOnly: true,
+    },
+  ],
+  extraContainerOptions: {
+    capabilities: {
+      adds: ["SYS_RAWIO", "SYS_ADMIN"],
+    },
+    devices: [
+      "sda",
+      "sdb",
+      "sdc",
+      "sdd",
+      "sde",
+      "sdf",
+      "sdg",
+      "sdh",
+      "sdi",
+      "sdj",
+      "nvme0",
+      "nvme1",
+    ].map((i) => ({
+      containerPath: `/dev/${i}`,
+      hostPath: `/dev/${i}`,
+      permissions: "r",
+    })),
+  },
+  middlewares: ["auth"],
+});
